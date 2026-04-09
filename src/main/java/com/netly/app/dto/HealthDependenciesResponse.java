df1ca88454707bb2a2a0c3639ce65fa4101cd5ea
Yogesh Sonawane <yogeshsonawane@Mac.lan>
2026-04-09 14:14:45 +0530

    health api added

--- src/main/java/com/netly/app/dto/HealthDependenciesResponse.java
+++ src/main/java/com/netly/app/dto/HealthDependenciesResponse.java
@@ -3,7 +3,7 @@ package com.netly.app.dto;
 import java.util.Map;
 
 public record HealthDependenciesResponse(
-        String app,
+        String status,
         Map<String, String> dependencies
 ) {
 }
